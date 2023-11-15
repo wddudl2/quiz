55daa7057317b98e3c395d330635c5d773afaba0
wddudl2 <akht258@gmail.com>
2023-11-15 07:16:57 +0530

    3rd commit

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/get-quiz/', views.get_quiz, name="get_quiz"),
+    path('quiz/', views.quiz, name="quiz"),
 
     path('admin/', admin.site.urls),
     
--- home/views.py
+++ home/views.py
@@ -1,20 +1,25 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 import random
 import json
 
-
-
 def home(request):
-    
-    context = {'categories' : Category.objects.all}
-    return render(request, 'home.html' ,context)
+    context = {'categories': Category.objects.all()}
+    if request.GET.get('category'):
+        return redirect(f"/quiz/?category={request.GET.get('category')}")
+    return render(request, 'home.html', context)
 
+def quiz(request):
+    return render(request, 'quiz.html')
 
 def get_quiz(request):
     try:
         question_objs = list(Question.objects.all())
+        if request.GET.get('category'): 
+            question_objs = question_objs.filter(category__category_name__icontains=request.GET.get('category'))
+
+        question_objs = list(question_objs)
+
         data = []
 
         random.shuffle(question_objs)
